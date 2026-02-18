678803d74674c76895ad3f73c98ec9706afa37f7
ashupublicgitrepo <ashu.publicgitrepo@gmail.com>
2026-02-18 08:16:23 +0530

    local storage added

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
-const inputInfo = document.getElementById("info");
-const addButton = document.getElementById("addBtn");
-const contentOnPage = document.getElementById("listDiv");
-addButton.addEventListener("click", addHandler);
-const setOfData = [];
+const info = document.getElementById("info");
+const infoAddButton = document.getElementById("addInfoBtn");
+const contentPage = document.getElementById("contentPage");
+const localCleanerButton = document.getElementById("localCleaner");
+infoAddButton.addEventListener("click", addHandler);
+localCleanerButton.addEventListener("click", cleaner);
+const setOfInfo = [];
+function localSaver(index, data) {
+  localStorage.setItem(index, data);
+  renderer();
+}
+function localDeleter(index) {
+  localStorage.removeItem(index);
+  renderer();
+}
+function infoPusher(info) {
+  setOfInfo.push(info);
+  //    here we only have to save those information which is recieved just now, and not map all the setofinfo again and again, to prevent duplicate key creation,
+  localSaver(setOfInfo.length, setOfInfo[setOfInfo.length - 1]);
+}
 
-function dataPusher(data) {
-    setOfData.push(data);
-    shower();
+function renderer() {
+  contentPage.textContent = null;
+  // huston here we have problem, now if we are adding in local storage, it is a object, which has key value pair, now, we passed key as index of our set of info array, so we have to access by key,
+  for (const keyNumber of Object.keys(localStorage).sort()) {
+    const textValue = localStorage.getItem(keyNumber);
+
+    // the main issue in this code is, first order is not maintained because object.keys return an array, but that array can be random, so i solved it by sort(), but next issue is local storage built in object, we are also iterating over it, and cant able to remove it from,yes i removed by using if statement. today task completed, it seems light but it took me around 3 hours. finelly done.
+    if (keyNumber != "bot-builder-storage") {
+      const secDiv = document.createElement("div");
+      const textDiv = document.createElement("p");
+      const deleteButton = document.createElement("button");
+      textDiv.setAttribute("style", "display:inline");
+      textDiv.textContent = keyNumber + " : " + textValue + "  ";
+      deleteButton.textContent = "delete";
+      deleteButton.addEventListener("click", (e) => divDeleter(e, keyNumber));
+      secDiv.appendChild(textDiv);
+      secDiv.appendChild(deleteButton);
+      contentPage.appendChild(secDiv);
+    }
+  }
+}
+
+function divDeleter(e, index) {
+  e.preventDefault();
+  localDeleter(index);
 }
 function addHandler(e) {
-    e.preventDefault();
-    const info = inputInfo.value.trim(); 
-    if (info.length < 1) return false;
-    dataPusher(info);
-    inputInfo.value = null;
+  e.preventDefault();
+  const data = info.value.trim();
+  if (data.length < 1) return false;
+  infoPusher(data);
+  info.value = null;
 }
-function shower() {
-    contentOnPage.textContent = null;
-    setOfData.map((content, index) => {
-        const infoDiv = document.createElement("div");
-        const newPera = document.createElement("p");
-        newPera.textContent = index+1 + " : " + content + "   ";
-        newPera.setAttribute("style", "display: inline");
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "delete";
-        infoDiv.appendChild(newPera);
-        infoDiv.appendChild(deleteButton);
-        deleteButton.addEventListener("click", (e)=>deleterOfInfo(e, index));
-        contentOnPage.appendChild(infoDiv);
-    })
+function cleaner(e) {
+  e.preventDefault();
+  localStorage.clear();
+  renderer();
 }
-function deleterOfInfo(event, index) {
-    event.preventDefault();
-    setOfData.splice(index, 1);
-    shower();
-    
-}
-// i didnt clean it yet, i just get command on showing a div with appending two childeren like peragraph element and button, and i today also learned that, delete button with extranal function when clicked, it delete the div, according to index. i also first time use a callback funtion in eventlistner, before this, i only use a extranal function and only write like click, name of function, but in it, i have to pass perameters, and that is called lifting state up, in react, althoug, i dont understand it yet, i mean that lifting state up, but, i did something new, this can be a birthplace of my new understanding of lifting state up, although now its hard to me to form a bridge between lifting state up in react and this call backfuntion in which i passed index. and i dont able to grasp why event is cut by line in code, whatever, it is working, i understand it, i did it without watching a tutorial, or except documention only for splice, now, today task you given is completed, but as, my energy is recharged, if i will have enough time during day, i will be back and do something which make me one step more closer to our crud app, our end goal is, add item, checked done, show listed item, checked done, delete item, checked done, persist in local storage, this is not a big deal, but unchecked, uncompleted, next objective is this, disable button while loading, unchecked, next to next objective, show error, end goal, after this, or may be at state where i persist into local storage, i will start this crud app, to clean, i mean one function one task, one output, somehow, i did it, not fully but partially, you can check and tell me, after i fully cleaned my code, i will make it automatic, means state, logic, renderer, in which state become one object for truth, only renderer touch the dom and logic is only extract data and update state, now tell me, what should i push on github, means should i push this code to track someone how i progress, or when i completed, my cleaing, means renderer, state, logic, part. or should i push like, one is uncleared but fully working, and one is full automatic, same but cleaned and automatic reusable like state, logic, render. you can suggest me. whatever, today task is done, many flaws but task is not clean it, first task is abstract the logic behind the app, i did it, when i achieved push in local storage so even site reloades, our list will be same, then i start to clean, and then i start to automate things, untill then, i will just enjoy abstraction. 
+renderer();
