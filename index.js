ba48835946c1721f4a4dbe257cbd6581dec40f8e
ashupublicgitrepo <ashu.publicgitrepo@gmail.com>
2026-02-18 10:29:02 +0530

    this version removes direct reading from local storage

--- index.js
+++ index.js
@@ -5,56 +5,77 @@ const localCleanerButton = document.getElementById("localCleaner");
 infoAddButton.addEventListener("click", addHandler);
 localCleanerButton.addEventListener("click", cleaner);
 const setOfInfo = [];
+
 function localSaver(index, data) {
-  localStorage.setItem(index, data);
-  renderer();
+    localStorage.setItem(index, data);
 }
 function localDeleter(index) {
-  localStorage.removeItem(index);
-  renderer();
+    localStorage.removeItem(index+1);
 }
 function infoPusher(info) {
-  setOfInfo.push(info);
-  //    here we only have to save those information which is recieved just now, and not map all the setofinfo again and again, to prevent duplicate key creation,
-  localSaver(setOfInfo.length, setOfInfo[setOfInfo.length - 1]);
+    setOfInfo.push(info);
+    localSaver(setOfInfo.length, setOfInfo[setOfInfo.length - 1]);
+    // although i have to mannualy set and send data by .length property.
+    renderer();
+    //  this infopusher function save data on both, on local variable and on local storage.
+    console.log(localStorage);
 }
-
-function renderer() {
-  contentPage.textContent = null;
-  // huston here we have problem, now if we are adding in local storage, it is a object, which has key value pair, now, we passed key as index of our set of info array, so we have to access by key,
-  for (const keyNumber of Object.keys(localStorage).sort()) {
-    const textValue = localStorage.getItem(keyNumber);
-
-    // the main issue in this code is, first order is not maintained because object.keys return an array, but that array can be random, so i solved it by sort(), but next issue is local storage built in object, we are also iterating over it, and cant able to remove it from,yes i removed by using if statement. today task completed, it seems light but it took me around 3 hours. finelly done.
-    if (keyNumber != "bot-builder-storage") {
-      const secDiv = document.createElement("div");
-      const textDiv = document.createElement("p");
-      const deleteButton = document.createElement("button");
-      textDiv.setAttribute("style", "display:inline");
-      textDiv.textContent = keyNumber + " : " + textValue + "  ";
-      deleteButton.textContent = "delete";
-      deleteButton.addEventListener("click", (e) => divDeleter(e, keyNumber));
-      secDiv.appendChild(textDiv);
-      secDiv.appendChild(deleteButton);
-      contentPage.appendChild(secDiv);
+function syncher() {
+    if (setOfInfo.length > 0) return false;
+    for (const key of Object.keys(localStorage).sort()) {
+        if (key != "bot-builder-storage") {
+            setOfInfo.push(localStorage.getItem(key));
+        };
+       
     }
-  }
+    localStorage.clear();
+    // this line clear local storage just after reloading or first time loading, but it first loads from local to current variable, and just after it sync again the local storage so, current page and local storage can be on same layer.  
+     setOfInfo.map((element, index) => {
+       localSaver(index + 1, element);
+     });
+    renderer();
+   
 }
-
+function renderer() {
+    contentPage.textContent = null; 
+    setOfInfo.map((info, num) => {
+        contentPainter(info, num);
+    }
+    )
+}
+   
 function divDeleter(e, index) {
-  e.preventDefault();
-  localDeleter(index);
+    e.preventDefault();
+    setOfInfo.splice(index, 1);
+    localDeleter(index);
+    console.log(setOfInfo);
+    console.log(localStorage);
+    // now to automate this, we need a function which automaticaly call localDeleter and localSaver, whenver we change data in setofInfo.
+    renderer();  
 }
 function addHandler(e) {
-  e.preventDefault();
-  const data = info.value.trim();
-  if (data.length < 1) return false;
-  infoPusher(data);
-  info.value = null;
+    e.preventDefault();
+    const data = info.value.trim();
+    if (data.length < 1) return false;
+    infoPusher(data);
+    info.value = null;
+
+}
+function contentPainter(info, num) {
+            const secDiv = document.createElement("div");
+            const textDiv = document.createElement("p");
+            const deleteButton = document.createElement("button");
+            textDiv.setAttribute("style", "display:inline");
+            textDiv.textContent = num + 1 + " " + " " + info;
+            deleteButton.textContent = "delete";
+            deleteButton.addEventListener("click", (e) => divDeleter(e, num));
+            secDiv.appendChild(textDiv);
+            secDiv.appendChild(deleteButton);
+            contentPage.appendChild(secDiv);
 }
 function cleaner(e) {
-  e.preventDefault();
-  localStorage.clear();
-  renderer();
+    e.preventDefault();
+    localStorage.clear();
 }
 renderer();
+syncher();
